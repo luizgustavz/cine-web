ce80d7621017d2198bfcddb1ddcc29e5088c6718
luizgustavz <luiz.gus.ms@gmail.com>
2025-10-20 14:57:54 -0300

    first commit

---
+++ src/main/java/br/com/cine_web/CineWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.cine_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cine_web/CineWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cine_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
